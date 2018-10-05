81c30181b632095b17e7b43bda778284ddbf2c99
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2018-10-05 10:45:06 +0100

    Initial commit

---
+++ presto-repo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Creating a stub github repository called: $1"
+
+# Silently create a new repository in GitHub/financial-times-sandbox
+curl -s -H "Authorization: token $(echo $GITHUB_API_TOKEN)" https://api.github.com/orgs/financial-times-sandbox/repos -d '{"name":"'"$1"'","description":"This repository is for testing & development purposes."}' > /dev/null
+
+mkdir $1
+cd $1
+git init
+git remote add origin git@github.com:financial-times-sandbox/$1.git
+cp ../.template/* ./
+echo "# 🎩 $1\n$(cat README.md)" > README.md;
+git add .
+git commit -m "🎩  Hey presto! A new GitHub repo!"
+git push
+
+echo "✅  Done."
