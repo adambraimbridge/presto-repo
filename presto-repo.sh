6e6a0ff5a24d01c166ab0cc9ed6156a46aadcd66
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2019-01-21 12:55:31 +0000

    Example for creating repos for github users (as opposed to orgs).

--- presto-repo.sh
+++ presto-repo.sh
@@ -2,16 +2,16 @@
 echo "Creating a stub github repository called: $1"
 
 # Silently create a new repository in GitHub/financial-times-sandbox
-curl -s -H "Authorization: token $(echo $GITHUB_API_TOKEN)" https://api.github.com/orgs/financial-times-sandbox/repos -d '{"name":"'"$1"'","description":"This repository is for testing & development purposes."}' > /dev/null
+curl -s -H "Authorization: token $(echo $GITHUB_API_TOKEN)" https://api.github.com/user/repos -d '{"name":"'"$1"'","description":"This repository is for testing & development purposes."}' > /dev/null
 
 mkdir $1
 cd $1
 git init
-git remote add origin git@github.com:financial-times-sandbox/$1.git
+git remote add origin git@github.com:adambraimbridge/$1.git
 cp ../.template/* ./
 echo "# 🎩 $1\n$(cat README.md)" > README.md;
 git add .
 git commit -m "🎩  Hey presto! A new GitHub repo!"
-git push
+git push -
 
 echo "✅  Done."
